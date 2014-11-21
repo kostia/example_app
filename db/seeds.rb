bcb4cb3edf5dd252fe56e612d40a6dd5958f627a
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-11-21 13:48:27 +0100

    Users with admin flag and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+%w[alice bob eve].each do |name|
+  email = "#{name}@scrivito.com"
+  next if User.find_by(email: email)
+
+  User.new(email: email).tap do |user|
+    user.password = 'testtest'
+    user.password_confirmation = 'testtest'
+    user.save!
+  end
+end
+
+User.find_by(email: 'alice@scrivito.com').update_attributes(admin: true)
