ba0c85f522ca7beeef7349f8a13a62372b81be70
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-11-21 13:00:03 +0100

    Require ActiveRecord

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_model/railtie"
-# require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
