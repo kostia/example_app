13adbf76d5ad6abb2ba67a9dcf231e3c535a23ee
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-11-24 12:16:11 +0100

    Fixed restrict obj publish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# Encoding: utf-8
+
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -6,11 +8,11 @@ class User < ActiveRecord::Base
 
   def to_scrivito_user
     Scrivito::User.define(email) do |scrivito_user|
-      scrivito_user.is_admin! if admin?
       scrivito_user.suggest_users { |input| User.search(input).map(&:to_scrivito_user) }
-      scrivito_user.restrict_obj_publish(using: :path) do |path|
-        'You can not publish the homepage' if path == '/' && !admin?
+      scrivito_user.restrict_obj_publish(using: :_path) do |path|
+        'Sie dürfen die Homepage nicht veröffentlichen.' if path == '/' && !admin?
       end
+      scrivito_user.is_admin! if admin?
     end
   end
 end
--- config/initializers/scrivito.rb
+++ config/initializers/scrivito.rb
@@ -1,4 +1,13 @@
 Scrivito.configure do |config|
-  config.editing_auth { |env| env['warden'].user.try(:to_scrivito_user) }
-  config.find_user { |email| User.find_by(email: email).try(:to_scrivito_user) }
+  config.editing_auth do |env|
+    if user = env['warden'].user
+      user.to_scrivito_user
+    end
+  end
+
+  config.find_user do |email|
+    if user = User.find_by(email: email)
+      user.to_scrivito_user
+    end
+  end
 end
