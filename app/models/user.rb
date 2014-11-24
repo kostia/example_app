1cada0b56509191cf49db66022874ae76ea5cad3
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-11-24 13:24:41 +0100

    Starting point: Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   searchkick
-
-  def to_scrivito_user
-    Scrivito::User.define(email) do |scrivito_user|
-      scrivito_user.suggest_users { |input| User.search(input).map(&:to_scrivito_user) }
-      scrivito_user.restrict_obj_publish(using: :_path) do |path|
-        'Sie dürfen die Homepage nicht veröffentlichen.' if path == '/' && !admin?
-      end
-      scrivito_user.is_admin! if admin?
-    end
-  end
 end
--- config/initializers/scrivito.rb
+++ config/initializers/scrivito.rb
@@ -1,13 +1,5 @@
 Scrivito.configure do |config|
   config.editing_auth do |env|
-    if user = env['warden'].user
-      user.to_scrivito_user
-    end
-  end
-
-  config.find_user do |email|
-    if user = User.find_by(email: email)
-      user.to_scrivito_user
-    end
+    Scrivito::User.system_user
   end
 end
