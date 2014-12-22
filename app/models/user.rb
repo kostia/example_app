011c73da74a1af00ca1cea7afb6d84c9af44bc9d
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2014-12-22 12:09:11 +0100

    Fixed user search

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   searchkick
+
+  def to_scrivito_user
+    Scrivito::User.define(email) do |scrivito_user|
+      scrivito_user.is_admin!
+
+      scrivito_user.suggest_users do |input|
+        self.class.search(input).map(&:to_scrivito_user)
+      end
+    end
+  end
 end
--- config/initializers/scrivito.rb
+++ config/initializers/scrivito.rb
@@ -1,5 +1,9 @@
 Scrivito.configure do |config|
   config.editing_auth do |env|
-    Scrivito::User.system_user
+    User.find_by(email: 'alice@scrivito.com').try(:to_scrivito_user)
+  end
+
+  config.find_user do |id|
+    User.find(id).try(:to_scrivito_user)
   end
 end
